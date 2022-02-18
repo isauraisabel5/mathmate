3a8a4b19853615561a10431fb11c5ad45ba83c27
isauraisabel5 <76772815+isauraisabel5@users.noreply.github.com>
2022-02-18 13:05:02 +0100

    Homepage done

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+//Hamburgermenu
+
 var Hamburger = document.querySelector("#Hamburger");
 Hamburger.addEventListener("click", showMenu, false);
  
@@ -16,3 +18,24 @@ function hideMenu(e) {
  
     document.body.style.overflow = "auto";
 }       
+
+
+
+// Dropdownmenu
+
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  } 
