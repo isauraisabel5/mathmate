98fac01faa4a479de7157e9ea2ef9be11c01705b
n1njayden <94377575+n1njayden@users.noreply.github.com>
2022-02-18 16:24:03 +0100

    html en js done

--- js/script.js
+++ js/script.js
@@ -39,3 +39,81 @@ function myFunction() {
       }
     }
   } 
+
+  function optellen() {
+    let num1 = parseInt(document.getElementById('num1-optellen').value);
+    let num2 = parseInt(document.getElementById('num2-optellen').value);
+    document.getElementById('answer1').innerHTML = num1 += num2;
+}
+
+document.getElementById('optellen').addEventListener('click', optellen);
+
+function aftrekken() {
+  let num1 = parseInt(document.getElementById('num1-aftrekken').value);
+  let num2 = parseInt(document.getElementById('num2-aftrekken').value);
+  document.getElementById('answer2').innerHTML = num1 - num2;
+}
+
+document.getElementById('aftrekken').addEventListener('click', aftrekken);
+
+function vermenigvuldigen() {
+  let num1 = parseInt(document.getElementById('num1-vermenigvuldigen').value);
+  let num2 = parseInt(document.getElementById('num2-vermenigvuldigen').value);
+  document.getElementById('answer3').innerHTML = num1 * num2;
+}
+
+document.getElementById('vermenigvuldigen').addEventListener('click', vermenigvuldigen);
+
+function delen() {
+  let num1 = parseInt(document.getElementById('num1-delen').value);
+  let num2 = parseInt(document.getElementById('num2-delen').value);
+  document.getElementById('answer4').innerHTML = num1 / num2;
+}
+
+document.getElementById('delen').addEventListener('click', delen);
+
+function delen() {
+  let num1 = parseInt(document.getElementById('num1-delen').value);
+  let num2 = parseInt(document.getElementById('num2-delen').value);
+  document.getElementById('answer4').innerHTML = num1 / num2;
+}
+
+document.getElementById('delen').addEventListener('click', delen);
+
+function macht() {
+  let num1 = parseInt(document.getElementById('num1-macht').value);
+  let num2 = parseInt(document.getElementById('num2-macht').value);
+  document.getElementById('answer5').innerHTML = num1 ** num2;
+}
+
+document.getElementById('macht').addEventListener('click', macht);
+
+function percentage() {
+  let num1 = parseInt(document.getElementById('num1-percentage').value);
+  let num2 = parseInt(document.getElementById('num2-percentage').value);
+  document.getElementById('answer6').innerHTML = 100 * num1 / num2;
+}
+
+document.getElementById('percentage').addEventListener('click', percentage);
+
+function restwaarde() {
+  let num1 = parseInt(document.getElementById('num1-restwaarde').value);
+  let num2 = parseInt(document.getElementById('num2-restwaarde').value);
+  document.getElementById('answer7').innerHTML = num1 % num2; 
+}
+
+document.getElementById('restwaarde').addEventListener('click', restwaarde);
+
+function even() {
+  let num1 = parseInt(document.getElementById('num1-even').value);
+  if(num1 % 2 == 0) {
+    document.getElementById('answer8').innerHTML = "even"; 
+}else {
+    document.getElementById('answer8').innerHTML = "odd"; 
+}
+}
+
+document.getElementById('even').addEventListener('click', even);
+
+
+
