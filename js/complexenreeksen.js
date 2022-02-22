719e99abd7c494db6fe4bea787f8b011ff5d6603
n1njayden <94377575+n1njayden@users.noreply.github.com>
2022-02-22 16:51:55 +0100

    rip mijn brein

--- js/complexenreeksen.js
+++ js/complexenreeksen.js
@@ -1,1 +1,21 @@
-//Priemgetallen
+//Fibonacci
+function printFibonacci(){
+    const num = document.getElementById('fibonacci-value1');
+    let n1 = 0, n2 = 1, nextTerm;
+
+    document.getElementById('answer13').innerHTML = n1;
+    document.getElementById('answer13').innerHTML = n2;
+
+    nextTerm = n1 + n2;
+
+/*
+    while(nextTerm <=  num){
+        document.getElementById('answer13').innerHTML = nextTerm;
+        
+        n1 = n2;
+        n2 = nextTerm;
+        nextTerm = n1 + n2;
+    }
+}
+
+document.getElementById('fibonacci').addEventListener('click', printFibonacci);
